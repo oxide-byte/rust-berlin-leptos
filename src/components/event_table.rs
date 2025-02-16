e18b7386fa0a4c6089273bf025beeaf952e76a69
oxide-byte <oxidebyte@gmail.com>
2025-02-16 14:03:10 +0100

    Add Thaw-UI

--- src/components/event_table.rs
+++ src/components/event_table.rs
@@ -1,61 +1,76 @@
+use leptos::logging::log;
 use leptos::prelude::*;
 use crate::models::event::generate_demo;
+use thaw::*;
 
 #[component]
 pub fn EventTable() -> impl IntoView {
     let data = RwSignal::new(generate_demo(10));
+    let page = RwSignal::new(1);
+    let filter_domain = RwSignal::new(String::from(""));
+    let filter_title = RwSignal::new(String::from(""));
+    let filter_url = RwSignal::new(String::from(""));
+    let filter_description = RwSignal::new(String::from(""));
+
+    let effect_page = Effect::watch(
+        move || page.get(),
+              move |new_page, _, _| {
+                  // Query new Page
+                  log!("Load new page [{}]", new_page);
+                  }, false, );
+
     view! {
-          <div class="w-full">
-              <table class="w-full table-auto">
-                  <thead>
-                    <tr>
-                      <th>
+          <div class="w-full mt-2 mb-2">
+              <Table class="w-full table-auto">
+                  <TableHeader>
+                    <TableRow>
+                      <TableHeaderCell>
                         <div class="grid grid-flow-col grid-rows-2">
                             <div>Domain</div>
-                            <div class="border ml-1 mr-1"><input/></div>
+                            <div class="border ml-1 mr-1"><Input value=filter_domain/></div>
                         </div>
-                      </th>
-                      <th>
+                      </TableHeaderCell>
+                      <TableHeaderCell>
                         <div class="grid grid-flow-col grid-rows-2">
                             <div>Title</div>
-                            <div class="border ml-1 mr-1"><input/></div>
+                            <div class="border ml-1 mr-1"><Input value=filter_title/></div>
                         </div>
-                      </th>
-                      <th>
+                      </TableHeaderCell>
+                      <TableHeaderCell>
                         <div class="grid grid-flow-col grid-rows-2">
                             <div>URL</div>
-                            <div class="border ml-1 mr-1"><input/></div>
+                            <div class="border ml-1 mr-1"><Input value=filter_url/></div>
                         </div>
-                      </th>
-                      <th>
+                      </TableHeaderCell>
+                      <TableHeaderCell>
                         <div class="grid grid-flow-col grid-rows-2">
                             <div>Description</div>
-                            <div class="border ml-1 mr-1"><input/></div>
+                            <div class="border ml-1 mr-1"><Input value=filter_description/></div>
                         </div>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
+                      </TableHeaderCell>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
                     <For
                         each=move || data.get()
                         key=|item| item.id.clone()
                         let:event
                     >
-                        <tr>
-                            <td>{{event.title}}</td>
-                            <td>{{event.domain}}</td>
-                            <td><a href={{event.url.clone()}} class="font-medium text-blue-600 dark:text-blue-500 hover:underline"> {{event.url.clone()}} </a></td>
-                            <td>{{event.description}}</td>
-                        </tr>
+                        <TableRow>
+                            <TableCell>{{event.title}}</TableCell>
+                            <TableCell>{{event.domain}}</TableCell>
+                            <TableCell><Link href=event.url.clone()>{{event.url.clone()}}</Link></TableCell>
+                            <TableCell>{{event.description}}</TableCell>
+                        </TableRow>
                      </For>
-                  </tbody>
+                  </TableBody>
                   <tfoot>
-                      <tr>
-                        <td>Showing....</td>
-                        <td>Pages....</td>
-                      </tr>
+                    <Space vertical=true>
+                        <div>"Page: " {move || page.get()}</div>
+                        <Pagination page page_count=10 />
+                    </Space>
                   </tfoot>
-              </table>
+              </Table>
           </div>
       }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ mod models;
 
 use leptos::prelude::*;
 use crate::components::app::App;
+use thaw::*;
 
 fn main() {
-    mount_to_body(|| view! { <App/> })
+    mount_to_body(|| view! {
+        <ConfigProvider>
+            <App/>
+        </ConfigProvider>
+    })
 }
